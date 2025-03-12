9477084086cd226dfcacd084aeae8125c0768f3f
catboybinary <catboybinary@disroot.org>
2025-03-13 02:27:47 +0300

    Added compatibility with Ars Nouveau

--- neoforge/src/main/java/meow/binary/o123456789/neoforge/mixin/MEStorageScreenMixin.java
+++ neoforge/src/main/java/meow/binary/o123456789/neoforge/mixin/MEStorageScreenMixin.java
@@ -19,27 +19,7 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(MEStorageScreen.class)
 public class MEStorageScreenMixin {
-    @Unique
-    private static void o123456789$renderSizeLabel(Matrix4f matrix, Font fontRenderer, float xPos, float yPos, Component text, float scaleFactor) {
-        RenderSystem.disableBlend();
-        float x = (xPos + 16.0F) / scaleFactor - fontRenderer.width(text);
-        float y = (yPos + 16.0F) / scaleFactor - 7;
-        MultiBufferSource.BufferSource buffer = Minecraft.getInstance().renderBuffers().bufferSource();
-        fontRenderer.drawInBatch(text, x, y, 16777215, true, matrix, buffer, Font.DisplayMode.NORMAL, 0, 15728880);
-        buffer.endBatch();
-        RenderSystem.enableBlend();
-    }
 
-    @Unique
-    private static void o123456789$renderSizeLabel(GuiGraphics guiGraphics, Font fontRenderer, float xPos, float yPos, Component text) {
-        float scaleFactor = Math.min(1.0F, 15F / (float)fontRenderer.width(text));
-        PoseStack stack = guiGraphics.pose();
-        stack.pushPose();
-        stack.translate(0.0F, 0.0F, 200.0F);
-        stack.scale(scaleFactor, scaleFactor, 1f);
-        o123456789$renderSizeLabel(stack.last().pose(), fontRenderer, xPos, yPos, text, scaleFactor);
-        stack.popPose();
-    }
 
     @Redirect(method = "renderSlot",
             at = @At(
@@ -50,6 +30,6 @@ public class MEStorageScreenMixin {
             require = 0
     )
     private void redirected(GuiGraphics guiGraphics, Font fontRenderer, float xPos, float yPos, String text, boolean largeFonts) {
-        o123456789$renderSizeLabel(guiGraphics, fontRenderer, xPos, yPos, Component.literal(text).withStyle(Style.EMPTY.withFont(O123456789.FONT)));
+        O123456789.renderSizeLabel(guiGraphics, fontRenderer, xPos, yPos, Component.literal(text).withStyle(Style.EMPTY.withFont(O123456789.FONT)));
     }
 }
