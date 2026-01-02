958860ac2e87c8f7747d6a32623f3afe34025833
evanbones <74276616+evanbones@users.noreply.github.com>
2026-01-02 10:59:36 -0800

    Use wrap operation instead of redirect, fixes #2

--- common/src/main/java/meow/binary/o123456789/mixin/GuiGraphicsMixin.java
+++ common/src/main/java/meow/binary/o123456789/mixin/GuiGraphicsMixin.java
@@ -1,5 +1,7 @@
 package meow.binary.o123456789.mixin;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
 import meow.binary.o123456789.O123456789;
 import net.minecraft.client.gui.Font;
@@ -8,19 +10,21 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.Style;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(GuiGraphics.class)
 public abstract class GuiGraphicsMixin {
-    @Redirect(
+
+    @WrapOperation(
             method = "renderItemDecorations(Lnet/minecraft/client/gui/Font;Lnet/minecraft/world/item/ItemStack;IILjava/lang/String;)V",
             at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphics;drawString(Lnet/minecraft/client/gui/Font;Ljava/lang/String;IIIZ)I")
     )
-    private int redirected(GuiGraphics instance, Font font, String text, int x, int y, int color, boolean dropShadow,
-                           @Local(ordinal = 0, argsOnly = true) int itemX,
-                           @Local(ordinal = 1, argsOnly = true) int itemY
-    ) {
+    private int wrapDrawString(GuiGraphics instance, Font font, String text, int x, int y, int color, boolean dropShadow,
+                               Operation<Integer> original,
+                               @Local(ordinal = 0, argsOnly = true) int itemX,
+                               @Local(ordinal = 1, argsOnly = true) int itemY) {
+
         Component component = Component.literal(text).withStyle(Style.EMPTY.withFont(O123456789.FONT));
+
         return instance.drawString(font, component, itemX + 16 - font.width(component), y, color);
     }
 }
