59649facceff9e453cbd9d2419959a96d0eb0875
evanbones <74276616+evanbones@users.noreply.github.com>
2026-01-03 06:24:13 -0800

    Improve bigger stacks compatibility

--- common/src/main/java/meow/binary/o123456789/mixin/GuiGraphicsMixin.java
+++ common/src/main/java/meow/binary/o123456789/mixin/GuiGraphicsMixin.java
@@ -15,8 +15,34 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.text.DecimalFormat;
+
 @Mixin(value = GuiGraphics.class, priority = 4096)
 public abstract class GuiGraphicsMixin {
+
+    private static final DecimalFormat BILLION_FORMAT  = new DecimalFormat("#.##B");
+    private static final DecimalFormat MILLION_FORMAT  = new DecimalFormat("#.##M");
+    private static final DecimalFormat THOUSAND_FORMAT = new DecimalFormat("#.##K");
+
+    private static final double ONE_BILLION = 1_000_000_000.0;
+    private static final double ONE_MILLION = 1_000_000.0;
+    private static final double ONE_THOUSAND = 1_000.0;
+
+    private static String getShortenedStackCount(int count) {
+        var decimal = new BigDecimal(count).round(new MathContext(3));
+        var value = decimal.doubleValue();
+
+        if (value >= ONE_BILLION)
+            return BILLION_FORMAT.format(value / ONE_BILLION);
+        else if (value >= ONE_MILLION)
+            return MILLION_FORMAT.format(value / ONE_MILLION);
+        else if (value > ONE_THOUSAND)
+            return THOUSAND_FORMAT.format(value / ONE_THOUSAND);
+
+        return String.valueOf(count);
+    }
 
     @Inject(
             method = "renderItemDecorations(Lnet/minecraft/client/gui/Font;Lnet/minecraft/world/item/ItemStack;IILjava/lang/String;)V",
@@ -30,15 +56,28 @@ public abstract class GuiGraphicsMixin {
 
             String stringToRender = text;
             if (stringToRender == null && stack.getCount() != 1) {
-                stringToRender = String.valueOf(stack.getCount());
+                stringToRender = getShortenedStackCount(stack.getCount());
             }
 
             if (stringToRender != null) {
                 pose.pushPose();
-                pose.translate(0.0F, 0.0F, 200.0F);
 
                 Component component = Component.literal(stringToRender).withStyle(Style.EMPTY.withFont(O123456789.FONT));
-                self.drawString(font, component, x + 19 - 2 - font.width(component), y + 6 + 3, 16777215, true);
+
+                int textWidth = font.width(component);
+
+                float scale = 1.0F;
+                if (textWidth > 16) {
+                    scale = 16.0F / (float) textWidth;
+                }
+
+                pose.translate(0.0F, 0.0F, 200.0F);
+                pose.scale(scale, scale, 1.0F);
+
+                float xPos = (x + 17) / scale - textWidth;
+                float yPos = (y + 9) / scale;
+
+                self.drawString(font, component, (int) xPos, (int) yPos, 16777215, true);
 
                 pose.popPose();
             }
