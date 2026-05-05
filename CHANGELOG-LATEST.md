1e7f4a86f46cc47d4705d7ecb94472175512d557
evanbones <74276616+evanbones@users.noreply.github.com>
2026-05-05 13:15:45 -0700

    2.0.3

--- CHANGELOG-LATEST.md
+++ CHANGELOG-LATEST.md
@@ -1,3 +1,3 @@
 ### Fixed
 
-- Fixed issues with numbers when using Sensible Stackables.
+- Fixed numbers being centered when using Sensible Stackables.
--- common/src/main/java/com/evandev/reliable_recount/mixin/GuiGraphicsMixin.java
+++ common/src/main/java/com/evandev/reliable_recount/mixin/GuiGraphicsMixin.java
@@ -14,24 +14,32 @@ import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(GuiGraphics.class)
 public abstract class GuiGraphicsMixin {
+
     @WrapOperation(
             method = "renderItemDecorations(Lnet/minecraft/client/gui/Font;Lnet/minecraft/world/item/ItemStack;IILjava/lang/String;)V",
             at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphics;drawString(Lnet/minecraft/client/gui/Font;Ljava/lang/String;IIIZ)I")
     )
-    private int wrapDraw(GuiGraphics instance, Font font, String text, int strX, int strY, int color, boolean dropShadow, Operation<Integer> original, @Local(ordinal = 0, argsOnly = true) int itemX, @Local(ordinal = 1, argsOnly = true) int itemY) {
+    private int wrapDraw(GuiGraphics instance, Font font, String text, int strX, int strY, int color, boolean dropShadow, Operation<Integer> original, @Local(ordinal = 0, argsOnly = true) int itemX) {
         ModConfig.FontStyle style = ModConfig.get().fontStyle;
-
         if (style == ModConfig.FontStyle.VANILLA) {
             return original.call(instance, font, text, strX, strY, color, dropShadow);
         }
 
         Component styled = CommonClass.getStyledAmount(text);
 
+        int vanillaWidth = font.width(text);
+        int customWidth = font.width(styled);
+        int widthDiff = vanillaWidth - customWidth;
+
         if (Services.PLATFORM.isModLoaded("sensible_stackables")) {
-            return CommonClass.drawStringWrapper(instance, font, styled, strX, strY, color);
+            boolean isModifiedByStackables = (text.length() > 2 || !text.chars().allMatch(Character::isDigit));
+
+            if (isModifiedByStackables) {
+                return CommonClass.drawStringWrapper(instance, font, styled, strX + widthDiff, strY, color);
+            }
         }
 
         int xOffset = (style == ModConfig.FontStyle.CREATE) ? 16 : 17;
-        return CommonClass.drawStringWrapper(instance, font, styled, itemX + xOffset - font.width(styled), strY, color);
+        return CommonClass.drawStringWrapper(instance, font, styled, itemX + xOffset - customWidth, strY, color);
     }
 }
